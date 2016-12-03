7b64d9968de1c2a51d0e98ad1e985255dbd9cbe7
RyanWan <ryanwan@Ryans-MacBook-Pro.local>
2016-12-02 21:25:28 -0600

    get reqeust for submit

--- Map/Map/SubmitViewController.swift
+++ Map/Map/SubmitViewController.swift
@@ -11,6 +11,11 @@ import UIKit
 class SubmitViewController: UIViewController {
     
     var bus: String!
+    var apiKey = "AIzaSyA-zNnojD_2V81XAKp-AShKGNb32bRwtfI"
+    var lat:String!
+    var lng:String!
+    var address:String!
+
 
     @IBOutlet weak var addressTextField: UITextField!
     @IBOutlet weak var nameLabel: UILabel!
@@ -26,26 +31,66 @@ class SubmitViewController: UIViewController {
     }
     
     @IBAction func submitAddress(sender: AnyObject) {
-        let def = NSUserDefaults.standardUserDefaults()
-        
-        var array1: [NSString] = [NSString]()
-        array1.append("value 1")
-        array1.append("value 2")
-        
-        //save
-        var defaults = NSUserDefaults.standardUserDefaults()
-        defaults.setObject(array1, forKey: bus)
-        defaults.synchronize()
-        
-        //read
-        if let testArray : AnyObject? = defaults.objectForKey(bus) {
-            var readArray : [NSString] = testArray! as! [NSString]
-            print(readArray)
-        }
-        
+        let address = addressTextField.text?.stringByReplacingOccurrencesOfString(" ", withString: "+") as String?
+        self.loadAndSendData(address!)
         let detailedVC = MapViewController(nibName: "MapViewController", bundle: nil)
         navigationController?.pushViewController(detailedVC, animated: true)
     }
+    
+    func loadAndSendData(searchText:String){
+        dispatch_async(dispatch_get_global_queue(QOS_CLASS_USER_INITIATED,0)){
+           self.fetchData(searchText)
+              dispatch_async(dispatch_get_main_queue()){
+              var url:NSURL?
+              print("http://localhost:5000/submit/\(self.bus)/\(self.address)/\(self.lat),\(self.lng)")
+              url = NSURL(string: "http://localhost:5000/submit/\(self.bus)/\(self.address)/\(self.lat),\(self.lng)")
+              let request = NSURLRequest(URL: url!)
+              var data:NSData
+              do {
+                  data = try NSURLConnection.sendSynchronousRequest(request, returningResponse: nil)
+                  print ("Finished")
+                    //                    let json = JSON(data: data)
+                    //                    print("json is \(json)")
+                    //                    var lists = json["address"]
+                    //                    destinations.removeAll()
+                    //
+                    //                    for item in lists{
+                    //                        destinations.append(String(item))
+                    //                    }
+                   } catch{
+                      print ("FAULTED")
+                   }
+               }
+           }
+      }
+
+    func fetchData(searchText:String){
+        let json = getJSON("https://maps.googleapis.com/maps/api/geocode/json?address=\(searchText)&key=\(self.apiKey)")
+        let jsonContent = json["results"].arrayValue
+
+        if (jsonContent.count != 0){
+            let result = jsonContent[0]
+            self.address =  result["formatted_address"].stringValue.stringByReplacingOccurrencesOfString(" ", withString: "+") as String?
+            self.lat = result["geometry"]["location"]["lat"].stringValue
+            self.lng = result["geometry"]["location"]["lng"].stringValue
+        }else {
+            print("no result found")
+        }
+    }
+
+    private func getJSON(url: String) -> JSON {
+       if let nsurl = NSURL(string: url){
+          if let data = NSData(contentsOfURL: nsurl) {
+             let json = JSON(data: data)
+                return json
+             } else {
+                return nil
+              }
+          } else {
+            return nil
+          }
+        }
+
 
     /*
     // MARK: - Navigation
